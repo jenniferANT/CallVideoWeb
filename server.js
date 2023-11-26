e37f49e6b213273095a428ece6c9d2b4a57b3e46
jenniferANT <nghuynhduyenjenf@gmail.com>
2023-11-26 20:16:54 +0700

    on

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+
+//Express initializes app to be a function handler that you can supply to an HTTP serve
+
 const { v4: uuidv4 } = require("uuid");
+
+// Create uuid
+
 const { ExpressPeerServer } = require("peer");
 const io = require("socket.io")(server, {
   cors: {
     origin: '*'
   }
 });
+
 
 const opinions = {
   debug: true,
@@ -18,6 +25,7 @@ app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  //We define a route handler / that gets called when we hit our website home.
   res.redirect(`/${uuidv4()}`);
 });
 
@@ -27,7 +35,7 @@ app.get("/:room", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
-    socket.join(roomId);
+    socket.join(roomId); 
 
     setTimeout(() => {
       io.to(roomId).emit("user-connected", userId);
@@ -40,7 +48,15 @@ io.on("connection", (socket) => {
 });
 
 const port = process.env.PORT || 3030;
+//const ip = '192.168.1.53';
 
 server.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
+
+// const port = process.env.PORT || 3030;
+
+// server.listen(port, '0.0.0.0', () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
